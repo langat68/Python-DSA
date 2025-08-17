e024a9115d7ed1972e846b56ac4874612c1f082d
langat68 <langatfidel3@gmail.com>
2025-08-17 23:33:53 +0300

    first commit

---
+++ array.py
@@ -0,0 +1,15 @@
+arr = [10, 20, 30, 40]
+
+# Traversal
+for x in arr:
+    print(x)
+
+# Insert
+arr.append(50)
+
+# Delete
+arr.remove(30)
+
+# Search
+if 20 in arr:
+    print("Found 20")
---
+++ stack.py
@@ -0,0 +1,9 @@
+stack = []
+
+# Push
+stack.append(10)
+stack.append(20)
+
+# Pop
+print(stack.pop())  # 20
+print(stack.pop())  # 10
